f68ba2fd24ed56abe29dfca85880e0e5a6ed3b25
Matthew Cliatt <mtcliatt@valdosta.edu>
2016-09-27 18:03:41 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,127 @@
+'use strict';
+
+
+// Color and thickness of the grid's lines
+const gridColor = 'grey';
+const gridThickness = 2;
+
+const rectangleActiveColor = 'white';
+const rectangleInactiveColor = 'black';
+
+// The number of rectangles in the grid
+const numRectanglesWide = 106;
+const numRectanglesHigh = 17;
+
+// The total size of the grid lines
+const verticalLineSize = numRectanglesWide * gridThickness;
+const horizontalLineSize = numRectanglesHigh * gridThickness;
+
+(() => {
+
+  // Find the canvas, use the 2d context to draw on it
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+
+  // .9 and .4 were chosen so the grid wasn't too stretched out
+  canvas.width = .95 * window.innerWidth;
+  canvas.height = .3 * window.innerHeight;
+
+  // The room left for the rectangles is the canvas size excluding the room
+  // needed to draw the grid lines.
+  const rectangleWidth = (canvas.width - verticalLineSize) / numRectanglesWide;
+  const rectangleHeight = (canvas.height - horizontalLineSize) / numRectanglesHigh;
+
+  const grid = [];
+
+  // Initialize the grid with all false values, i.e. the inactive state
+  for (let i = 0; i < 106; i++) {
+    grid.push([]);
+    for (let j = 0; j < 17; j++) {
+      grid[i].push(false);
+    }
+  }
+
+  const rectangleToCoordinate = (x, y) => {
+
+  }
+
+  const coordinateToRectangle = (x, y) => {
+    
+  }
+
+  const drawLine = (x1, y1, x2, y2) => {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+  };
+
+  const drawRectangle = (x, y, color) => {
+    ctx.clearRect(x, y, rectangleWidth, rectangleHeight);
+
+    const rectangle = new Path2D();
+    rectangle.rect(x, y, rectangleWidth, rectangleHeight);
+    ctx.fillStyle = color;
+    ctx.fill(rectangle);
+  }
+
+  const drawGrid = (() => {
+    ctx.strokeStyle = gridColor;
+    ctx.lineWidth = gridThickness;
+
+    // Since we draw the lines on the left and top sides of each rectangle,
+    // we need to use <= instead of < so that the last border is drawn
+    for (let i = 0; i <= numRectanglesWide; i++) {
+      const x = i * rectangleWidth + i * gridThickness;
+      drawLine(x, 0, x, canvas.height);
+    }
+
+    for (let i = 0; i <= numRectanglesHigh; i++) {
+      const y = i * rectangleHeight + i * gridThickness;
+      drawLine(0, y, canvas.width, y);
+    }
+
+    for (let i = 0; i < numRectanglesWide; i++) {
+      for (let j = 0; j < numRectanglesHigh; j++) {
+        const x = i * rectangleWidth + i * gridThickness + 1;
+        const y = j * rectangleHeight + j * gridThickness + 1;
+        drawRectangle(x, y, rectangleInactiveColor);
+      }
+    }
+  })();
+
+  const toggleRectangle = (x, y) => {
+
+    let color;
+    grid[x][y] = !grid[x][y];
+
+    if (grid[x][y]) {
+      color = rectangleActiveColor;
+    } else {
+      color = rectangleInactiveColor;
+    }
+
+    drawRectangle()
+
+  }
+
+
+  // When a rectangle is clicked, toggle its state (active or not active)
+  canvas.addEventListener('click', evt => {
+
+    // Get the position of the mouse, relative to the top left of the canvas
+    const rect = canvas.getBoundingClientRect();
+    const x = evt.clientX - rect.left;
+    const y = evt.clientY - rect.top;
+
+    const rx = Math.ceil(x / (rectangleWidth + gridThickness) - gridThickness / 2);
+    const ry = Math.ceil(y / (rectangleHeight + gridThickness) - gridThickness / 2);
+
+    toggleRectangle(rx, ry);
+
+    //console.log(`Clicked at: ${x}, ${y}`);
+    //console.log(`Clicked rectangle: ${rx}, ${ry}`);
+
+  });
+
+})();
